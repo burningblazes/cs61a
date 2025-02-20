7ebb29d9e036db4a1914c2f3406037ac7aa02dcd
Yusheng Yang <yusheng@Mac.attlocal.net>
2025-02-19 19:46:41 -0600

    hw08 finished till Q3

--- hws/hw08/hw08.scm
+++ hws/hw08/hw08.scm
@@ -1,8 +1,37 @@
 ; Q1
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    (if
+        (<= (length s) 1) 
+        #t
+        (let 
+            ((x (car s)) (y (car(cdr s))) )
+            (if (<= x y) (ascending? (cdr s)) #f)
+        )    
+    ))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+; Q2
+(define (my-filter pred s) 
+    (cond 
+        ((null? s) '())
+        ((pred (car s))  (cons (car s)  (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+        ))
 
+
+; Q3
+(define (interleave lst1 lst2) 
+    (cond 
+    ((and (null? lst1) (null? lst2)) nil)
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else 
+        (cons (car lst1) 
+            (cons (car lst2)
+                (interleave (cdr lst1) (cdr lst2)) )))
+    ))
+
+
+
+; Q4
 (define (no-repeats s) 'YOUR-CODE-HERE)
