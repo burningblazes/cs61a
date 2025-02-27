b0446f982596feb7834a67ada1140bdece35e379
Yusheng Yang <yusheng@Mac.attlocal.net>
2025-02-27 15:56:12 -0600

    scheme done except EC

--- projects/scheme/questions.scm
+++ projects/scheme/questions.scm
@@ -6,9 +6,11 @@
 ;; Problem 15
 ;; Returns a list of two-element lists
 (define (enumerate s)
-  ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
+  (define (helper lst index)
+    (if (null? lst) 
+        '()  
+        (cons (list index (car lst)) (helper (cdr lst) (+ 1 index)))))  
+  (helper s 0))
   ; END PROBLEM 15
 
 ;; Problem 16
@@ -17,8 +19,14 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
-  )
+  ( cond 
+      ((null? s1) s2  )
+      ((null? s2) s1) 
+      ((ordered? (car s1) (car s2) )
+              (cons (car s1) (merge ordered? (cdr s1) s2)))
+      (else
+           (cons (car s2) (merge ordered? s1 (cdr s2))))
+  )  )
   ; END PROBLEM 16
 
 ;; Optional Problem 2
